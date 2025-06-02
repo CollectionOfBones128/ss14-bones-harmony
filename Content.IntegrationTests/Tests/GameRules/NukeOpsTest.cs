f958c098af487a91f90dc7a002c28966cdedf87f
CollectionOfBones128 <150093917+CollectionOfBones128@users.noreply.github.com>
2025-06-02 11:18:25 +0100

    should fix nukies testfail for real, ported from deltav

--- Content.IntegrationTests/Tests/GameRules/NukeOpsTest.cs
+++ Content.IntegrationTests/Tests/GameRules/NukeOpsTest.cs
@@ -230,9 +230,12 @@ public sealed class NukeOpsTest
         for (var tick = 0; tick < totalTicks; tick += increment)
         {
             await pair.RunTicksSync(increment);
-            // Harmony Change Start, Allows IPCs to be Nukies I think
-            if (!entMan.HasComponent<SiliconComponent>(player)) // Goobstation
+            // Harmony Change Start, Allows IPCs to be Nukies I think, or at least makes the game not cry when it thinks about them being Nukies
+            if (!entMan.HasComponent<SiliconComponent>(player)) // Goobstation - IPC
+            {
+                var resp = entMan.GetComponent<RespiratorComponent>(player);
                 Assert.That(resp.SuffocationCycles, Is.LessThanOrEqualTo(resp.SuffocationCycleThreshold));
+            }
             Assert.That(damage.TotalDamage, Is.EqualTo(FixedPoint2.Zero));
             // Harmony Change End
         }
